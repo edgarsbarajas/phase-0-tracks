05f7b788a00361d658e4ba085fe6241e48b0f430
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-06-22 13:05:28 -0700

    Add an interface

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -4,6 +4,7 @@
   Loop through string using an index.
   If char is a space, let it be.
   Otherwise for every index, asign itself with .next method.
+  Implicitly return word
 =end
 
 def encrypt(word)
@@ -30,6 +31,7 @@ end
   Index through the word.
   If char is a space, leave it alone.
   Otherwise, revert to prev char.
+  Implicitly return word
 =end
 
 def decrypt(word)
@@ -50,10 +52,32 @@ def decrypt(word)
   word
 end
 
-# driver code
-p encrypt("abc")
-p encrypt("zed")
-p decrypt("bcd")
-p decrypt("afe")
-p decrypt(encrypt("swordfish")) #Encrypt method implicitly returns a modified "swordfish" string. The decrypt method modifies it back to normal, implicitly returning "swordfish"
+=begin
+  # driver code
+  p encrypt("abc")
+  p encrypt("zed")
+  p decrypt("bcd")
+  p decrypt("afe")
+  p decrypt(encrypt("swordfish")) #Encrypt method implicitly returns a modified "swordfish" string. The decrypt method modifies it back to normal, implicitly returning "swordfish"
+=end
+
+# Ask user if they want to encrypt or decrypt a password
+# Ask for the password
+# Encrypt or decrypt, depending on request
+
+puts "Would you like to encrypt or decrypt a password?"
+choice = gets.chomp
+
+puts "Enter a password: "
+password = gets.chomp
+
+puts "Result: "
+
+if choice == "encrypt"
+  puts encrypt(password)
+elsif choice == "decrypt"
+  puts decrypt(password)
+else
+  puts "Could not process your request"
+end
 
