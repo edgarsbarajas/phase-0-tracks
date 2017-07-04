b28c56ae7012cc98e9f5f8986675c4fbf094368d
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-07-04 13:03:27 -0700

    Ask user to update any keys in hash, and print client info

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -28,3 +28,29 @@ puts "Name: #{client[:name]}"
 puts "Age: #{client[:age]}"
 puts "Number of children: #{client[:number_of_children]}"
 puts "Decor theme: #{client[:decor_theme]}"
+
+puts "\nEnter key to update or 'none' to continue."
+key_name = gets.chomp
+
+case key_name
+when "name"
+  print "Enter updated client name: "
+  client[key_name.to_sym] = gets.chomp
+when "age"
+  print "Enter updated client age: "
+  client[key_name.to_sym] = gets.chomp.to_i
+when "number_of_children"
+  print "Enter updated client child count: "
+  client[key_name.to_sym] = gets.chomp.to_i
+when "decor_theme"
+  print "Enter updated client decor theme: "
+  client[key_name.to_sym] = gets.chomp
+when "none"
+  puts "No updates."
+end
+
+puts "\nCLIENT INFORMATION"
+puts "Name: #{client[:name]}"
+puts "Age: #{client[:age]}"
+puts "Number of children: #{client[:number_of_children]}"
+puts "Decor theme: #{client[:decor_theme]}"
