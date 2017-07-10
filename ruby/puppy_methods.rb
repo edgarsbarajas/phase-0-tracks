b1865a826c60b35fc0d4851654370232ce265d22
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-07-09 21:36:04 -0700

    Add driver code at the bottom that initializes an instance of Puppy, a speak method, a roll_over method,  a dog_years method that takes an integer (of human years) and converts that number to dog years, and a sit method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,31 @@ class Puppy
     toy
   end
 
+  def speak(num)
+    while num > 0
+      puts "Woof!"
+      num -= 1
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    years = human_years * 7
+    years
+  end
+
+  def sit
+    puts "*sits*"
+  end
 end
 
+fido = Puppy.new
+
+fido.fetch("ball")
+fido.speak(5)
+fido.roll_over
+p fido.dog_years(6)
+fido.sit
