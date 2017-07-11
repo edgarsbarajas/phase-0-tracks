d6e4dec3c788870f90508733c0f1650ace94c1dd
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-07-11 10:50:21 -0700

    Define Santa class, speak method, eat milk and cookies method and Initialize method

---
+++ ruby/santa.rb
@@ -0,0 +1,13 @@
+class Santa
+  def speak
+    puts "Ho, ho ,ho!, Haaaaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(type_of_cookie)
+    puts "That was a good #{type_of_cookie}"
+  end
+
+  def Initialize
+    puts "Initializing Santa instance..."
+  end
+end
