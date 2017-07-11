14a64ce64822551def94dbeb1291203371617962
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-07-11 11:39:25 -0700

    Initialize a bunch of Santa instances using example gender and ethnicity arrays. Print their about

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,12 @@
 class Santa
+  def initialize(gender, ethnicity)
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+    puts "Initializing Santa instance..."
+  end
+
   def speak
     puts "Ho, ho ,ho!, Haaaaaappy holidays!"
   end
@@ -7,11 +15,24 @@ class Santa
     puts "That was a good #{type_of_cookie}"
   end
 
-  def Initialize
-    puts "Initializing Santa instance..."
+  def about
+    puts "Santa is a #{@ethnicity} #{@gender}"
   end
 end
 
-Bob = Santa.new
+Bob = Santa.new("Male", "Mexican")
 Bob.speak
 Bob.eat_milk_and_cookies("snickerdoodle")
+Bob.about
+
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+santas = []
+
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+  santas[i].about
+end
+
+
+
