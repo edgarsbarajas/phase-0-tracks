a0bbd298e2f665663552b694e25e0844f82948e1
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-07-13 17:15:50 -0700

    Recreate mixin Shout module, define 2 new classes that include Shout, call both Module methods on both instances

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
@@ -10,3 +11,35 @@ end
 
 p Shout.yell_happily("Hello")
 p Shout.yell_angrily("Hell no")
+=end
+
+# Make a mixin version of the Shout module instead.
+
+module Shout
+def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def yell_happily(words)
+    words + "!!!" + " :) :)"
+  end
+end
+
+# Write two classes representing anything that might shout, and include the Shout module in those classes.
+class Human
+  include Shout
+end
+
+class Vampire
+  include Shout
+end
+
+# Test your work by adding driver code at the bottom of the file that instantiates instances of your classes and calls the two module methods on each instance.
+
+human = Human.new
+p human.yell_happily("Hello")
+p human.yell_angrily("NO")
+
+vampire = Vampire.new
+p vampire.yell_happily("Blood")
+p vampire.yell_angrily("Garlic")
