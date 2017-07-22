adfbf8e2afdcc08ee9209f3b675cf0405d94636b
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-07-21 22:06:46 -0700

    Add for loop to add horse name/color combos to horses object

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,17 @@ var horseNames = ["Ed", "Eddy", "Sean", "Peaches"]
 
 colors.push("black")
 horseNames.push("Bonnie")
+
+// add code that will use your two arrays to create an object. The
+// keys of your object should be horse names,
+// and the values should be colors. Your solution should be
+// something that would work for any number of colors/horses,
+// as long as the two arrays are the same length
+
+horses = {}
+
+for(var i = 0; i < horseNames.length; i++){
+  horses[horseNames[i]] = colors[i]
+}
+
+console.log(horses)
