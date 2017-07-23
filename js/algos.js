aa2af3734f34bf7cb7fdcbdaae8e0627d9200665
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-07-23 11:20:17 -0700

    write a function that takes two objects and checks to see if the objects share at least one key-value pair

--- js/algos.js
+++ js/algos.js
@@ -7,6 +7,14 @@
 // Continue to loop until all strings in array has been checked
 // return longest string
 
+// FindMatches function: takes in 2 objects
+// iterate through the first object:
+  // within the loop, iterate through the second object to compare the first object key to second object keys
+    // If there is a match, compare their values
+      // If the values match, a key-value match has been found
+// Return whether or not a key-value match was found
+
+
 function FindLongestString(arrayOfStrings){
   var longestStringLength = 0;
   var longestString = "";
@@ -21,6 +29,24 @@ function FindLongestString(arrayOfStrings){
   return longestString;
 }
 
+function findMatches(object1, object2){
+  var match = false;
+
+  for(var key1 in object1){
+    for(var key2 in object2){
+      if(key1 == key2){
+        if(object1[key1] == object2[key2]){
+          match = true;
+          break;
+        }
+      }
+    }
+  }
+
+  return match;
+}
+
+// Driver for FindLongestString
 var s = FindLongestString(["long phrase","longest phrase","longer phrase"]);
 console.log(s);
 
@@ -29,3 +55,17 @@ console.log(s);
 
 s = FindLongestString(["one","two","three", "fourfivesix"]);
 console.log(s);
+
+// Driver for findMatches
+var match = findMatches({name: "Steven", age: 54}, {name: "Tamir", age: 54});
+console.log(match); //true
+
+match = findMatches({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3});
+console.log(match); //true
+
+match = findMatches({boo: "Dog", legs: 4}, {hoo: "Dog", wheels: 3});
+console.log(match); //false
+
+
+
+
