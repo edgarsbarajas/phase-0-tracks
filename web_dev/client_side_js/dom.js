282a7fe06de0d86a49bee485a4759d12119bb573
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-08-02 19:17:26 -0700

    create new HTML elements with JavaScript and add them to your page, or how to loop through a collection of nodes of a certain class and apply the same effect to all of them

---
+++ web_dev/client_side_js/dom.js
@@ -0,0 +1,28 @@
+console.log("Script is working!")
+
+function addPinkBorder(event) {
+  event.target.style.border = "5px dotted pink"
+  console.log("Successfully added a pink border!");
+}
+
+var dude = document.getElementById("cool-dude");
+dude.addEventListener("click", addPinkBorder);
+
+var button = document.getElementById("button");
+button.addEventListener("click", function doCoolStuff(){
+  document.getElementsByTagName("h1")[0].innerHTML = "NEW HEADER WHO DIS"
+  console.log("changed the header!")
+
+  var catPicture = document.createElement("IMG");
+  catPicture.setAttribute("src", "cat.jpg");
+
+  document.body.children[2].appendChild(catPicture);
+  console.log("Added a cat! lol")
+})
+
+var test = document.getElementsByClassName("test");
+
+for(var i = 0; i < test.length; i++){
+  test[i].style.background = "pink";
+}
+
